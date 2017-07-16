8412c9b978377990d0b9d02ea13e2439079f125a
Collin <pineapplepie4@yahoo.com>
2017-07-16 14:06:52 -0700

    added working alerts for submit button

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,10 @@ $(function()  {
     var text = "";
     var userInput= parseInt($("#ping").val());
     alert(userInput)
-    for (var i= 0; userInput > i; userInput -=1 ) {
-      alert(i)
+    for (var i= 0; userInput > i; i -= 1 ) {
+      if ( userInput > i)   {
+
+      } break = 0
       text += i + "<br>";
       alert (userInput)
     }
