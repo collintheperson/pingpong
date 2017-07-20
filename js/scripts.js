f1b06938f5e3827c049a5f4b7f28365002ffc1d1
Collin <pineapplepie4@yahoo.com>
2017-07-19 22:59:11 -0700

    added some if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,27 @@
 //Back end
   var allNumbers = []
   var text = "";
-
-
-
-
 //Front End
 $(function()  {
   $("#ping-pong").submit(function(event)  {
     var userInput= parseInt($("#ping").val());
-    alert(userInput)
 
     for (; userInput > 0; userInput -= 1)  {
-      alert(userInput);
-      text += "The number is " + userInput + "<br>";};
-    }
+      console.log(userInput);
+      // userInput = allNumbers
+       $("#result").append(userInput);
+       if (userInput % 15) {
+         $("#result").append("pingpong");
+       }
+       else if (userInput % 3) {
+         $("#result").append("ping");
+       }
+       else if (userInput % 5) {
+         $("#result").append("pong");
+       };
 
-      //   userInput -= 1
-      //   console.log(allNumbers);
-      //   console.log(userInput)
-      //   console.log(i);
-      //   alert (i)
-      //
-      //
-      // };
-    //
-    //   } break = 0
-      // text += i + "<br>";
-    // };
+       };
+       event.preventDefault();
 
-      // if ( userInput % 3 ) {
-      //
-      // }
-    event.preventDefault();
   });
 });
