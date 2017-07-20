9e456643764df75f68b3c604c08a53d3381e678a
Collin <pineapplepie4@yahoo.com>
2017-07-19 21:30:21 -0700

    have a countdown from whatever number you choose

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,33 @@
+//Back end
+  var allNumbers = []
+  var text = "";
 
+
+
+
+//Front End
 $(function()  {
   $("#ping-pong").submit(function(event)  {
-
-    var text = "";
     var userInput= parseInt($("#ping").val());
     alert(userInput)
-    for (var i= 0; userInput > i; i -= 1 ) {
-      if ( userInput > i)   {
 
-      } break = 0
-      text += i + "<br>";
-      alert (userInput)
+    for (; userInput > 0; userInput -= 1)  {
+      alert(userInput);
+      text += "The number is " + userInput + "<br>";};
     }
+
+      //   userInput -= 1
+      //   console.log(allNumbers);
+      //   console.log(userInput)
+      //   console.log(i);
+      //   alert (i)
+      //
+      //
+      // };
+    //
+    //   } break = 0
+      // text += i + "<br>";
+    // };
 
       // if ( userInput % 3 ) {
       //
