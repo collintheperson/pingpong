0676d951a7969c736d648e00e1d40dbb367d2093
Collin <pineapplepie4@yahoo.com>
2017-07-21 19:49:36 -0700

    add begining steps to have user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 //Back end
-  // var allNumbers = []
-  // var text = "";
 
   function pingPong(balls) {
     var pongBalls = [];
@@ -18,36 +16,20 @@
     }
     return pongBalls;
 }
-console.log('pongBalls');
-var returnedArray = pingPong(12);
-console.log(returnedArray);
+var returnedArray = pingPong("");
 
-//FRONT END 
-for(var paddeles = 0; paddeles<returnedArray.length; paddeles +=1) {
-  $('#thelistitem').append(returnedArray[paddeles]);
+//FRONT END
+
+$(function()  {
+  $("#ping-pong").submit(function(event) {
+    event.preventDefault();
+    console.log(returnedArray);
+      // $("#result").text(returnedArray);
+      var userInput = parseInt($("#ping-pong").val());
+      console.log(userInput);
+      for(var paddeles = 0; paddeles<returnedArray.length; paddeles +=1) {
+        $('#thelistitem').append(returnedArray[paddeles]);
 }
-//what do i want to do with i? make it equal to userInput.
-  // if (i % 15 === 0) {
-  //   $(allNumbers).push("pingpong");
-  // }
-  // else if (i % 5 === 0) {
-  //   $(allNumbers).push("pong");
-  // }
-  // else if (i % 3 === 0) {
-  // $(allNumbers).push("ping");
-  // }
-// }
-  //  else if ((userInput % 15 === 0) || (userInput % 5 === 0) || (userInput % 3 === 0)) {
-//Front End
-// $(function()  {
-//   $("#ping-pong").submit(function(event)  {
-//       // console.log(allNumbers);
-//       var userInput= parseInt($("#ping").val());
-//       text.push(userInput);
-//       console.log(text)
-//       //how do i make this go through the for loop. I'm getting the userInput into a value.
-//         $("#result").append(allNumbers);
-//        event.preventDefault();
-// pingpong(userInput)
-//   });
-// });
+  });
+  // ("#ping-pong").reset();
+});
