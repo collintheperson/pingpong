340f5e4c96d8001a89b7fe6c3142eed06511ecc2
Collin <pineapplepie4@yahoo.com>
2017-07-22 23:06:37 -0700

    show loop of user input going up from number they input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 //Back end
-
-  function pingPong(balls) {
-    var pongBalls = [];
+function pingPong(balls) {
+  var pongBalls = [];
     for (var i = 1; i <= balls; i++) {
       if (i % 15 === 0){
         pongBalls.push("pingpong");
@@ -11,25 +10,21 @@
         pongBalls.push('pong');
       } else {
         pongBalls.push(i);
-      }
+      };
 
-    }
-    return pongBalls;
-}
-var returnedArray = pingPong("");
-
+    };
+  return pongBalls;
+};
 //FRONT END
 
 $(function()  {
   $("#ping-pong").submit(function(event) {
     event.preventDefault();
-    console.log(returnedArray);
-      // $("#result").text(returnedArray);
-      var userInput = parseInt($("#ping-pong").val());
-      console.log(userInput);
-      for(var paddeles = 0; paddeles<returnedArray.length; paddeles +=1) {
-        $('#thelistitem').append(returnedArray[paddeles]);
-}
+      var userInput = parseInt($("#ping").val());
+      var outPut = pingPong(userInput);
+      for(var paddles = 0; paddles<outPut.length; paddles += 1) {
+      $('#result').append("<li>" + outPut[paddles]);
+    };
+    return outPut;
   });
-  // ("#ping-pong").reset();
 });
